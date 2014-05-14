3efbd46080e1e6fbc7a668153861289e6e8eb8da
Rene Torres <Rene Torres@Rene>
2014-05-14 09:42:03 -0500

    mi primer comit

---
+++ src/ventana/Ventana.java
@@ -0,0 +1,16 @@
+
+package ventana;
+import mx.lasalle.poo.PanelPrincipal;
+import mx.lasalle.poo.Principal;
+
+public class Ventana {
+
+    public static void main(String[] args) {
+       
+        
+        
+        VentanaPrincipal ventana1 = new VentanaPrincipal();
+       ventana1.setVisible(true);
+               
+    }
+}
